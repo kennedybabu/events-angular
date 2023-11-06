a2e0115683fbba640a8ee8e3ecda6cf726e20a25
kennedybabu <kennedynjogu8@gmail.com>
2023-11-06 19:50:58 +0300

    feature: add the search and buttons components to the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     NavbarComponent,
-    UserCardComponent
+    UserCardComponent,
+    ButtonsComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
