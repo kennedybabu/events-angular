8743f9b6db5ee6b3c7defd51069d0a9d5f959c85
kennedybabu <kennedynjogu8@gmail.com>
2023-12-08 08:59:55 +0300

    display comment authors avatars

--- src/app/components/comment-obj/comment-obj.component.ts
+++ src/app/components/comment-obj/comment-obj.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comment-obj',
   templateUrl: './comment-obj.component.html',
   styleUrls: ['./comment-obj.component.scss']
 })
-export class CommentObjComponent {
+export class CommentObjComponent implements OnInit {
   @Input() comment!: any
+  url!: any
+  DJANGO_SERVER = 'http://127.0.0.1:8000'
+
+  ngOnInit(): void {
+    if(this.comment == null) {
+      this.url = this.comment?.author?.avatar
+    } else {
+      this.url = `${this.DJANGO_SERVER}${this.comment?.author?.avatar}`
+    }
+  }
+  
 
 }
